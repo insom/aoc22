73915774c8164e6a54fd19c58f7a51ed18ff91f0
Aaron Brady <aaron.brady@shopify.com>
2022-12-20 22:38:01 -0500

    Day1, Part1 and Part2 in Clojure

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject aoc22 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.10.0"]])
