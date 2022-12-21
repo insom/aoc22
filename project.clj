8be92b110fbcbe8b5f1b08ba7e27717b5b258d35
Aaron Brady <aaron.brady@shopify.com>
2022-12-21 11:31:21 -0500

    Day2, Part1 in Clojure

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.10.0"]])
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/core.match "1.0.0"]])
